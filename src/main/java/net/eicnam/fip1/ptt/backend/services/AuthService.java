a55413b70c68e0870c39007c6aeb0013c6e8d944
Fanny <feber@capcod.eu>
2022-12-16 10:21:32 +0100

    last change

--- src/main/java/net/eicnam/fip1/ptt/backend/services/AuthService.java
+++ src/main/java/net/eicnam/fip1/ptt/backend/services/AuthService.java
@@ -1,25 +1,20 @@
 package net.eicnam.fip1.ptt.backend.services;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import net.eicnam.fip1.ptt.backend.controllers.AuthController;
-import net.eicnam.fip1.ptt.backend.models.RUser;
-import net.eicnam.fip1.ptt.backend.utils.JWTDecode;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import lombok.AllArgsConstructor;
 
-import net.eicnam.fip1.ptt.backend.utils.DotEnvReader;
-import net.eicnam.fip1.sdk.rainbow.RainbowApplication;
+import net.eicnam.fip1.ptt.backend.models.RUser;
 import net.eicnam.fip1.ptt.backend.repositories.UserRepository;
-import net.eicnam.fip1.sdk.rainbow.authentication.RainbowAuthentication;
+import net.eicnam.fip1.ptt.backend.utils.DotEnvReader;
+import net.eicnam.fip1.ptt.backend.utils.JWTDecode;
 
+import net.eicnam.fip1.sdk.rainbow.RainbowApplication;
+import net.eicnam.fip1.sdk.rainbow.authentication.RainbowAuthentication;
 
 @AllArgsConstructor
 @Service
 public class AuthService {
-    private static final Logger LOGGER = LoggerFactory.getLogger(AuthService.class);
     private final UserRepository userRepository;
 
     public String login(final String email, final String password) {
@@ -35,15 +30,8 @@ public class AuthService {
                 env.getValue("API_APP_SECRET")
         ).getToken();
 
-        final String payload = JWTDecode.decode(token);
-
-        final ObjectMapper objectMapper = new ObjectMapper();
-        try {
-            final String userId = objectMapper.readTree(payload).get("user").get("id").asText();
-            userRepository.save(new RUser(token, userId));
-        } catch (final Exception e) {
-            LOGGER.error("login() > Error while parsing response to JSON: {}", e.getMessage());
-        }
+        final String userId = JWTDecode.getId(token);
+        userRepository.save(new RUser(token, userId));
 
         return token;
     }
--- src/main/java/net/eicnam/fip1/ptt/backend/utils/JWTDecode.java
+++ src/main/java/net/eicnam/fip1/ptt/backend/utils/JWTDecode.java
@@ -1,10 +1,26 @@
 package net.eicnam.fip1.ptt.backend.utils;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import net.eicnam.fip1.ptt.backend.models.RUser;
+import net.eicnam.fip1.ptt.backend.services.AuthService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.Base64;
 
 public class JWTDecode {
-    public static String decode(final String token) {
+    private static final Logger LOGGER = LoggerFactory.getLogger(JWTDecode.class);
+    public static String getId(final String token) {
         final String JWTPayload = token.split("\\.")[1];
-        return new String(Base64.getDecoder().decode(JWTPayload));
+        final String payload = new String(Base64.getDecoder().decode(JWTPayload));
+
+        final ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            return objectMapper.readTree(payload).get("user").get("id").asText();
+        } catch (final Exception e) {
+            LOGGER.error("getId() > Error while parsing response to JSON: {}", e.getMessage());
+        }
+
+        return null;
     }
 }
